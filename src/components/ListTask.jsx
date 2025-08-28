79f38d4296fa2ba47399028091331294d79d1c7f
viyan-najmaldin <najmadinviyan@gmail.com>
2025-08-28 20:42:24 +0300

    some edit

--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -8,27 +8,45 @@ function ListTask ({lists, setList}){
    const [vi,setVi] = useState('')
    const [idList,setIdList] = useState()
 
-    return( <div>
-        <ul> {lists.map((el,i)=>{
-    return <li key={i}>
-         <button type="button" className="edit btn" onClick={()=>{setVi(true); setIdList(i)}}>Edit</button>
-    {  (vi && idList===i)?  
-    ( <><input className="list" type="text" value={el} onChange={(e)=>{(setList(lists.map( (num,index) =>(index===i ? e.target.value : num) ) )) } }/> 
-      <button className="save btn" onClick={()=>{setVi(false); setIdList()}}><FontAwesomeIcon icon={faCheck} /></button></> ) 
-    : (<span key={i}> {el} </span>) }
-       
-    <button type="button" className="delete btn" onClick={()=>{setList(lists.filter((_,x) => (x!==i)));}}><FontAwesomeIcon icon={faTrash} /></button>
-</li>
-             
-        })}  
-       </ul>  
+    return( 
+      <div>
+          <ul> {lists.map((el,i)=>{
+            return (
 
+            <li key={i}>
+                {/* Edit button  here */}
+                <input type="submit" className="edit btn" 
+                  onClick={()=>{setVi(true); setIdList(i)}} value="Edit"  />
 
-       
-    </div>
+                {/* If User Press the Edit Button    */}
+                { (vi && idList===i)?  
+                ( <>
+                    <input className="list" type="text" value={el} 
+                      onChange={(e)=>{(setList(lists.map( (num,index) =>(index===i ? e.target.value : num) ) )) } } 
+                      onKeyDown={ (e)=>{if (e.key === "Enter"){setVi(false); setIdList();}}} /> 
+                      
+                    {/* Save the Edit  */}
+                    <button className="save btn" 
+                      onClick={()=>{setVi(false); setIdList()}}>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </button>
+                </> ) 
+
+              //  The List Item Or if the User Dosen't Press the Edit button 
+                  : (<span key={i}> {el} </span>) }
+                
+              {/* Delete Button  */}
+            <button type="button" className="delete btn" 
+                onClick={()=>{setList(lists.filter((_,x) => (x!==i)));}}>
+              <FontAwesomeIcon icon={faTrash} />
+            </button>
         
+            </li>)
+          })}  
+         </ul>  
+       </div>    
+    
     );
-
 }
 
 export default ListTask;
---
+++ src/components/Viyan.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Viyan = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Viyan
